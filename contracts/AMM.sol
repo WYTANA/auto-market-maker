d861b91d6b60380417d1ebeea3db8638eaedb992
torback <tlorback@gmail.com>
2022-11-21 20:49:26 -0700

    Create amm contract and initial tests

---
+++ contracts/AMM.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./Token.sol";
+
+// Manage Pool
+// Manage Deposits
+// Facilitate Swaps
+// Manage Withdrawals
+
+contract AMM {
+    Token public token1;
+    Token public token2;
+
+    constructor(Token _token1, Token _token2) {
+        token1 = _token1;
+        token2 = _token2;
+    }
+}
---
+++ test/AMM.js
@@ -0,0 +1,38 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether")
+}
+
+const ether = tokens
+
+describe("AMM", () => {
+  let accounts, deployer, token1, token2, amm
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+
+    const Token = await ethers.getContractFactory("Token")
+    token1 = await Token.deploy(" TLO AMM Token", "AMM", "1000000")
+    token2 = await Token.deploy("fUSD Token", "fUSD", "1000000")
+
+    const AMM = await ethers.getContractFactory("AMM")
+    amm = await AMM.deploy(token1.address, token2.address)
+  })
+
+  describe("Deployment", () => {
+    it("has an address", async () => {
+      expect(amm.address).to.not.equal(0x0)
+    })
+
+    it("tracks token1 address", async () => {
+      expect(await amm.token1()).to.equal(token1.address)
+    })
+
+    it("tracks token2 address", async () => {
+      expect(await amm.token2()).to.equal(token2.address)
+    })
+  })
+})
