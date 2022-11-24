9c1d97c29c9528eff17b701aa177f819e71d6deb
torback <tlorback@gmail.com>
2022-11-23 21:43:08 -0700

    Create function to add liquidity, constant product maker model, mapping, and tests

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -13,8 +13,60 @@ contract AMM {
     Token public token1;
     Token public token2;
 
+    uint256 public token1Balance;
+    uint256 public token2Balance;
+    uint256 public K;
+
+    uint256 public totalShares;
+    mapping(address => uint256) public shares;
+    uint256 constant PRECISION = 10**18;
+
     constructor(Token _token1, Token _token2) {
         token1 = _token1;
         token2 = _token2;
     }
+
+    function addLiquidity(uint256 _token1Amount, uint256 _token2Amount)
+        external
+    {
+        // Deposit tokens
+        require(
+            token1.transferFrom(msg.sender, address(this), _token1Amount),
+            "transfer of token1 failed"
+        );
+        require(
+            token2.transferFrom(msg.sender, address(this), _token2Amount),
+            "transfer of token2 failed"
+        );
+
+        // LP shares
+        uint256 share;
+
+        // Issue shares
+        // First time LPs receive 100 shares
+        if (totalShares == 0) {
+            share = 100 * PRECISION;
+        } else {
+            uint256 share1 = (totalShares * _token1Amount) / token1Balance;
+            uint256 share2 = (totalShares * _token2Amount) / token2Balance;
+            require(
+                (share1 / 1) * 10**3 == (share2 / 1) * 10**3,
+                "must provide equal weights for pairs"
+            );
+            share = share1;
+        }
+
+        // Update shares
+        totalShares += share;
+        shares[msg.sender] += share;
+
+        // Manage pool
+        token1Balance += _token1Amount;
+        token2Balance += _token2Amount;
+        K = token1Balance * token2Balance;
+    }
+
+    // Determine how many token2 tokens must be deposited when depositing liquidity for token1
+
+    // Determine how many token1 tokens must be deposited when depositing liquidity for token2
 }
--- test/AMM.js
+++ test/AMM.js
@@ -8,16 +8,32 @@ const tokens = (n) => {
 const ether = tokens
 
 describe("AMM", () => {
-  let accounts, deployer, token1, token2, amm
+  let accounts, deployer, liquidityProvider
+  let token1, token2, amm
 
   beforeEach(async () => {
+    // Setup accounts
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    liquidityProvider = accounts[1]
 
+    // Deploy tokens
     const Token = await ethers.getContractFactory("Token")
     token1 = await Token.deploy(" TLO AMM Token", "AMM", "1000000")
     token2 = await Token.deploy("fUSD Token", "fUSD", "1000000")
 
+    // Send tokens to LP
+    let transaction = await token1
+      .connect(deployer)
+      .transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    transaction = await token2
+      .connect(deployer)
+      .transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    // Deploy AMM
     const AMM = await ethers.getContractFactory("AMM")
     amm = await AMM.deploy(token1.address, token2.address)
   })
@@ -33,6 +49,67 @@ describe("AMM", () => {
 
     it("tracks token2 address", async () => {
       expect(await amm.token2()).to.equal(token2.address)
+    })
+  })
+
+  describe("Token swaps", () => {
+    let amount, transaction, result
+
+    it("have been facilitated", async () => {
+      // Deployer approves 100k tokens
+      amount = tokens(100000)
+      transaction = await token1.connect(deployer).approve(amm.address, amount)
+      await transaction.wait()
+
+      transaction = await token2.connect(deployer).approve(amm.address, amount)
+      await transaction.wait()
+
+      // Deployer add liquidity
+      transaction = await amm.connect(deployer).addLiquidity(amount, amount)
+      await transaction.wait()
+
+      // Check that AMM receives tokens
+      expect(await token1.balanceOf(amm.address)).to.equal(amount)
+      expect(await token2.balanceOf(amm.address)).to.equal(amount)
+
+      expect(await amm.token1Balance()).to.equal(amount)
+      expect(await amm.token2Balance()).to.equal(amount)
+      expect(await amm.K()).to.be.greaterThan(amount)
+
+      // Check deployer has 100 shares
+      expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+      // Check pool has 100 total shares
+      expect(await amm.totalShares()).to.equal(tokens(100))
+
+      //////////////////////////////////////////////////
+      // LP adds liquidity
+      // LP approves 50k tokens
+      amount = tokens(50000)
+      transaction = await token1
+        .connect(liquidityProvider)
+        .approve(amm.address, amount)
+      await transaction.wait()
+
+      transaction = await token2
+        .connect(liquidityProvider)
+        .approve(amm.address, amount)
+      await transaction.wait()
+
+      // LP add liquidity
+      transaction = await amm
+        .connect(liquidityProvider)
+        .addLiquidity(amount, amount)
+      await transaction.wait()
+
+      // LP should have 50 shares
+      expect(await amm.shares(liquidityProvider.address)).to.equal(tokens(50))
+
+      // Verify deployer still has 100 shares
+      expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+      // Verify pool shares outstanding is 150
+      expect(await amm.totalShares()).to.equal(tokens(150))
     })
   })
 })
