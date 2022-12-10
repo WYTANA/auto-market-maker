c2f12caed46020caae91a18efffd1fb640e98e46
torback <tlorback@gmail.com>
2022-12-09 21:27:17 -0700

    Create redux store for provider, network, and account; update ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,23 @@
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from "react-bootstrap/Navbar"
 
-import logo from '../logo.png';
+import logo from "../images/hills.png"
 
 const Navigation = ({ account }) => {
   return (
-    <Navbar className='my-3'>
+    <Navbar className="my-3">
       <img
         alt="logo"
         src={logo}
-        width="40"
-        height="40"
+        width="80"
+        height="80"
         className="d-inline-block align-top mx-3"
       />
-      <Navbar.Brand href="#">Dapp University Template</Navbar.Brand>
+      <Navbar.Brand href="#">Black Hills Automated Market Maker</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          {account}
-        </Navbar.Text>
+        <Navbar.Text>{account}</Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
-  );
+  )
 }
 
-export default Navigation;
+export default Navigation
